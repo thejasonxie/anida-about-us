80d26ba6243b350e74c29f3562d4fd40ad01a330
Jason Xie <thejasonxie@gmail.com>
2022-07-15 15:59:31 -0400

    Setup layout and routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name("home");
+
+Route::get("/pages/view/about", function() {
+    return view("pages.about");
+})->name("about");
