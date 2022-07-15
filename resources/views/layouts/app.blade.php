b8e95b7b48a387f6ccd9975869bb0aa3d4e1a31c
Jason Xie <thejasonxie@gmail.com>
2022-07-15 16:18:58 -0400

    Setup CSS stylesheet and fronts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>ANIDA @yield("title")</title>
+  
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Raleway&display=swap" rel="stylesheet"> 
+  
+  <link rel="stylesheet" href="{{asset("css/style.css")}}">
 </head>
 <body>
 
