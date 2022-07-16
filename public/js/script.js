334f04aa472236c7531d0d1a759d0af989f7c7d9
Jason Xie <thejasonxie@gmail.com>
2022-07-15 22:42:54 -0400

    Added styling and functionality to navbar

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+(function () {
+    let menuHamburgerBtn = document.querySelector(".navbar-hamburger-btn");
+    let miniMenu = document.querySelector(".navbar-menu");
+
+    menuHamburgerBtn.addEventListener("click", toggleMiniMenu);
+    window.addEventListener("resize", handleMiniMenu);
+
+    function toggleMiniMenu() {
+        miniMenu.classList.toggle("display-none");
+    }
+
+    function handleMiniMenu() {
+        if (window.innerWidth > 640) {
+            miniMenu.classList.remove("display-none");
+        } else {
+            miniMenu.classList.add("display-none");
+        }
+    }
+})();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,29 +10,38 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Raleway&display=swap" rel="stylesheet"> 
   
-  <link rel="stylesheet" href="{{asset("css/style.css")}}">
+  <link rel="stylesheet" href="{{ asset("css/style.css") }}">
+  <script defer src="{{ asset("js/script.js") }}"></script>
 </head>
 <body>
   <header>
     <nav>
-      <section>
-      <a href="{{ route("home")}}">
-        <img src="{{ asset("images/Logo.png") }}" alt="ANIDA logo">
-      </a>
-      </section>
-      <section>
-        <ul>
-          <li><a href="{{ route("home") }}">Home</a></li>
-          <li><a href="{{ route("about") }}">About Us</a></li>
-        </ul>
-      </section>
-      <section>
-        <div>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </section>
+      <div class="container navbar-container">
+        <section class="navbar-header">
+          <div class="navbar-header-container">
+            <a href="{{ route("home")}}">
+              <img src="{{ asset("images/Logo.png") }}" alt="ANIDA logo">
+            </a>
+          </div>
+        </section>
+        <section class="navbar-menu">
+          <ul class="navbar-menu-list">
+            <li class="navbar-menu-item">
+              <a href="{{ route("home") }}">Home</a>
+            </li>
+            <li class="navbar-menu-item">
+              <a href="{{ route("about") }}">About Us</a>
+            </li>
+          </ul>
+        </section>
+        <section class="navbar-hamburger">
+          <button class="navbar-hamburger-btn">
+            <span class="hamburger-bar1"></span>
+            <span class="hamburger-bar1"></span>
+            <span class="hamburger-bar1"></span>
+          </button>
+        </section>
+      </div>
     </nav>
   </header>
 
