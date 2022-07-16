1b8f56fc1a34ba866322dbb7eae3088b2039fd36
Jason Xie <thejasonxie@gmail.com>
2022-07-15 23:17:41 -0400

    Added about images and information

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,12 +3,43 @@
 @section("title", "| About Us")
 
 @section("content")
-  <h1><span>About</span>&nbsp;<span>Us</span></h1>
+  
   <main>
-    <section class="banner"></section>
+    <section class="banner">
+      <div>
+        <img src="{{ asset("images/Banner.png") }}" alt="About us banner image">
+      </div>
+      <div>
+        <h1>
+          <span>About</span>
+          &nbsp;
+          <span>Us</span>
+        </h1>
+        <hr>
+        <p>At ANIDA, we are individuals who believe that it takes more than passion and being intentional to create a world of difference.</p>
+      </div>
+    </section>
     <section class="boxes container">
-      <div class="box1"></div>
-      <div class="box2"></div>
+      <div class="box1">
+        <div>
+          <img src="{{ asset("images/Box1.png") }}" alt="ANIDA's vision image">
+        </div>
+        <div>
+          <h2>Vision</h2>
+          <hr>
+          <p>Creating opportunity through access to education.</p>
+        </div>
+      </div>
+      <div class="box2">
+        <div>
+          <img src="{{ asset("images/Box2.png") }}" alt="ANIDA's mission image">
+        </div>
+        <div>
+          <h2>Mission</h2>
+          <hr>
+          <p>An international organization rooted in Christian values that creates opportunities through community-based projects for individuals facing a difficult reality.</p>
+        </div>
+      </div>
     </section>
   </main>
 @endsection
